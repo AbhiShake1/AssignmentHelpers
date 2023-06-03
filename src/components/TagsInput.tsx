ec2e2341fad79e51138ef1e1391adea1a38c0b9b
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-06-03 21:12:00 +0545

    increase maxlines

--- src/components/TagsInput.tsx
+++ src/components/TagsInput.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
-import {Chip, TextField} from "@mui/material";
+import {Chip} from "@mui/material";
 import {type NextPage} from "next";
+import {Textarea} from "@mui/joy";
+import {toast} from "react-hot-toast";
+import Typography from "@mui/joy/Typography";
 
 interface TagPropType {
     onChange: (selectedTags: string[]) => void,
@@ -18,32 +21,31 @@ const TagsInput: NextPage<TagPropType, { tags: [] }> = ({onChange, placeholder})
 
     function handleInputChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
         const val = event.target.value
-        setTxt(val)
         const newChips = val.length == 0 ? [] : val.split(' ')
+
+        if(newChips.length>3){
+            toast.error('Only upto 3 skills allowed')
+            return
+        }
+
+        setTxt(val)
         setChips(newChips)
         onChange(newChips)
     }
 
     return (
-        <TextField
-            fullWidth
-            label={placeholder}
-            variant="outlined"
-            value={txt}
-            onChange={handleInputChange}
-            InputProps={{
-                startAdornment: chips.length==0 ? undefined : chips.map(item => (
-                    <Chip
-                        key={item}
-                        tabIndex={-1}
-                        label={item}
-                        className='space-x-2 mx-2'
-                        onDelete={handleDelete(item)}
-                    />
-                )),
-            }}
-        />
-    );
+        <Textarea placeholder={placeholder} minRows={3} value={txt} onChange={handleInputChange} startDecorator={
+            chips.length == 0 ? undefined : chips.map(item => (
+                <Chip
+                    key={item}
+                    tabIndex={-1}
+                    label={item}
+                    className='space-x-2 mx-2'
+                    onDelete={handleDelete(item)}
+                />
+            ))
+        }/>
+    )
 }
 
 export default TagsInput
