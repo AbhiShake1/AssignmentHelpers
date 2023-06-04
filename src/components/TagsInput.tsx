04976117e58abe5f37a3783d7eb7d5266ed2443c
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-06-04 09:44:35 +0545

    fix tags input

--- src/components/TagsInput.tsx
+++ src/components/TagsInput.tsx
@@ -3,6 +3,7 @@ import {Chip, TextField} from "@mui/material";
 import {type NextPage} from "next";
 import {Input} from "@mui/joy";
 import {toast} from "react-hot-toast";
+import {MuiChipsInput} from "mui-chips-input";
 
 interface TagPropType {
     onChange: (selectedTags: string[]) => void,
@@ -12,41 +13,19 @@ interface TagPropType {
 
 const TagsInput: NextPage<TagPropType, { tags: [], required: false }> = ({onChange, placeholder, required}) => {
     const [chips, setChips] = useState<string[]>([])
-    const [txt, setTxt] = useState('')
 
-    const handleDelete = (item: string) => () => {
-        const newChips = structuredClone(chips).filter(c => c != item) || []
-        setChips(newChips)
-    }
-
-    function handleInputChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-        const val = event.target.value
-        const newChips = val.length == 0 ? [] : val.split(' ').filter(v => v.trim() != '')
-
+    function handleInputChange(newChips: string[]) {
         if (newChips.length > 3) {
             toast.remove()
             toast.error('Only upto 3 skills allowed')
             return
         }
 
-        setTxt(val)
         setChips(newChips)
         onChange(newChips)
     }
 
-    return (
-        <TextField required={required} fullWidth label={placeholder} value={txt} variant='outlined' onChange={handleInputChange} InputProps={{
-            startAdornment: chips.length == 0 ? undefined : chips.map(item => (
-                <Chip
-                    key={item}
-                    tabIndex={-1}
-                    label={item}
-                    className='space-x-2 mx-2'
-                    onDelete={handleDelete(item)}
-                />
-            ))
-        }}/>
-    )
+    return <MuiChipsInput value={chips} onChange={handleInputChange} variant='outlined' label={placeholder} required={required} inputValue={'sjao'}/>
 }
 
 export default TagsInput
