eb074a1887122639e16a74a334ccc5d1a14e2300
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-06-04 18:03:09 +0545

    add validation in multi step form

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -11,11 +11,17 @@ function Register() {
     const router = useRouter()
     const [desc, setDesc] = useState('')
     const [phone, setPhone] = useState('')
+    const [education, setEduction] = useState('')
+    const [skills, setSkills] = useState<string[]>([])
+    const [specialization, setSpecialization] = useState('')
     return (
         <div>
             <MultiStepForm onSubmit={() => void router.replace('/')} steps={[
                 {
                     step: 'Personal',
+                    onNext: () => {
+                        if (phone == '') throw new Error('Phone number is required')
+                    },
                     child: <div className='p-8 white shadow-2xl rounded-lg flex flex-col space-y-2'>
                         <TextField label="Phone" type='tel' variant="outlined" fullWidth value={phone}
                                    onChange={(e) => setPhone(e.target.value)} required/>
@@ -35,10 +41,16 @@ function Register() {
                 },
                 {
                     step: 'Professional',
+                    onNext: () => {
+                        if (skills.length == 0) throw new Error('Skills is required')
+                        if (specialization.length == 0) throw new Error('Skills is required')
+                    },
                     child: <div className='p-8 white shadow-2xl rounded-lg flex flex-col space-y-2'>
-                        <TextField label="Education" variant="outlined" fullWidth/>
-                        <TagsInput onChange={() => ''} label='Skills' limit={5} required/>
-                        <TextField label="Specialization" variant="outlined" fullWidth required/>
+                        <TextField label="Education" variant="outlined" fullWidth value={education}
+                                   onChange={e => setEduction(e.target.value)}/>
+                        <TagsInput onChange={setSkills} label='Skills' limit={5} required/>
+                        <TextField label="Specialization" variant="outlined" value={specialization}
+                                   onChange={e => setSpecialization(e.target.value)} fullWidth required/>
                     </div>
                 },
                 {
