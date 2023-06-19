73b0d9e8ef80fc143e0c73d0d061fce3cef876ee
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-06-19 19:33:41 +0545

    avoid repeating chat bar styles

--- src/pages/supportChats/index.tsx
+++ src/pages/supportChats/index.tsx
@@ -1,72 +1,18 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {api} from "~/utils/api";
-import {Button, createStyles, getStylesRef, Input, Loader, Navbar, rem} from '@mantine/core';
+import {Button, Input, Loader, Navbar} from '@mantine/core';
 import {IconSend, IconUser,} from '@tabler/icons-react';
 import type {Message} from '@prisma/client'
 import {toast} from "react-hot-toast";
 import pusher from "~/stores/pusher";
 import {Events} from "~/const/events";
 import {useAuth} from "@clerk/nextjs";
-
-const useStyles = createStyles((theme) => ({
-    header: {
-        paddingBottom: theme.spacing.md,
-        marginBottom: `calc(${theme.spacing.md} * 1.5)`,
-        borderBottom: `${rem(1)} solid ${
-            theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
-        }`,
-    },
-
-    footer: {
-        paddingTop: theme.spacing.md,
-        marginTop: theme.spacing.md,
-        borderTop: `${rem(1)} solid ${
-            theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
-        }`,
-    },
-
-    link: {
-        // ...theme.fn.focusStyles(),
-        display: 'flex',
-        alignItems: 'center',
-        textDecoration: 'none',
-        fontSize: theme.fontSizes.sm,
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
-        padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
-        borderRadius: theme.radius.sm,
-        fontWeight: 500,
-
-        '&:hover': {
-            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-            color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-
-            [`& .${getStylesRef('icon')}`]: {
-                color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-            },
-        },
-    },
-
-    linkIcon: {
-        ref: getStylesRef('icon'),
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
-        marginRight: theme.spacing.sm,
-    },
-
-    linkActive: {
-        '&, &:hover': {
-            backgroundColor: theme.fn.variant({variant: 'light', color: theme.primaryColor}).background,
-            color: theme.fn.variant({variant: 'light', color: theme.primaryColor}).color,
-            [`& .${getStylesRef('icon')}`]: {
-                color: theme.fn.variant({variant: 'light', color: theme.primaryColor}).color,
-            },
-        },
-    },
-}));
+import {useChatBarStyles} from "~/hooks/useChatBarStyles";
 
 function Index() {
     const [msgs, setMsgs] = useState<Message[]>([])
     const chats = api.chat.supportChats.useQuery()
-    const {classes, cx} = useStyles();
+    const {classes, cx} = useChatBarStyles();
     const [active, setActive] = useState('')
     const [text, setText] = useState('')
     const messagesContainerRef = useRef<HTMLDivElement>(null);
