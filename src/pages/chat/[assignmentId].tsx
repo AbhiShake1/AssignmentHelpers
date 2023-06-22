7ad2f9b26077b9fc8862cf523ce825255163553d
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-06-22 07:57:43 +0545

    add start bidding button

--- src/pages/chat/[assignmentId].tsx
+++ src/pages/chat/[assignmentId].tsx
@@ -1,19 +1,19 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {api} from "~/utils/api";
-import {Button, Input, Loader} from '@mantine/core';
-import {IconSend,} from '@tabler/icons-react';
-import type {Message} from '@prisma/client'
-import {toast} from "react-hot-toast";
+import React, { useEffect, useRef, useState } from 'react';
+import { api } from "~/utils/api";
+import { Button, Input, Loader } from '@mantine/core';
+import { IconSend, } from '@tabler/icons-react';
+import type { Message } from '@prisma/client'
+import { toast } from "react-hot-toast";
 import pusher from "~/stores/pusher";
-import {Events} from "~/const/events";
-import {useAuth} from "@clerk/nextjs";
-import {useRouter} from "next/router";
+import { Events } from "~/const/events";
+import { useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/router";
 
 function Index(props) {
     const router = useRouter()
     const assignmentId = parseInt(router.query.assignmentId?.toString() ?? '')
     const [msgs, setMsgs] = useState<Message[]>([])
-    const chatQuery = api.chat.chat.useQuery({assignmentId}, {
+    const chatQuery = api.chat.chat.useQuery({ assignmentId }, {
         onSuccess: data => setMsgs(data.messages)
     })
     const [text, setText] = useState('')
@@ -35,7 +35,7 @@ function Index(props) {
         pusher.unbind(idStr)
         pusher.subscribe(idStr).bind(Events.SEND_MESSAGE, (message: Message) => {
             setMsgs(msgs => [message, ...msgs])
-            messagesContainerRef.current?.scroll({behavior: "smooth", top: 0})
+            messagesContainerRef.current?.scroll({ behavior: "smooth", top: 0 })
         })
 
         return () => pusher.unsubscribe(chat.fromUserId)
@@ -49,10 +49,10 @@ function Index(props) {
     })
 
     useEffect(() => {
-        messagesContainerRef.current?.scroll({behavior: "smooth", top: 0})
+        messagesContainerRef.current?.scroll({ behavior: "smooth", top: 0 })
     }, [msgs])
 
-    if (!chatQuery.isSuccess) return <center><Loader/></center>
+    if (!chatQuery.isSuccess) return <center><Loader /></center>
 
     return (
         <div className='flex flex-row h-[80vh]'>
@@ -64,12 +64,12 @@ function Index(props) {
                         <div key={message.id} className='flex flex-col-reverse'>
                             {
                                 message.senderId != user.userId ? <div className='flex flex-row mb-1'>
-                                        <div
-                                            className='py-2 px-4 bg-blue-300 max-w-xl rounded-b-3xl rounded-tr-3xl'>{message.text}</div>
-                                        <div className='w-full'/>
-                                    </div> :
+                                    <div
+                                        className='py-2 px-4 bg-blue-300 max-w-xl rounded-b-3xl rounded-tr-3xl'>{message.text}</div>
+                                    <div className='w-full' />
+                                </div> :
                                     <div className='flex flex-row mb-1'>
-                                        <div className='w-full'/>
+                                        <div className='w-full' />
                                         <div
                                             className='py-2 px-4 bg-blue-300 max-w-xl rounded-t-3xl rounded-bl-3xl'>
                                             {message.text}
@@ -80,17 +80,27 @@ function Index(props) {
                     ))
                 }
                 <Input value={text} onChange={e => setText(e.target.value)} placeholder='Write something..'
-                       size='lg' className='m-4 absolute bottom-4 w-8/12' rightSectionWidth={96}
-                       rightSection={
-                           <Button variant='subtle' disabled={!text} loading={sendMutation.isLoading}
-                                   onClick={() => sendMutation.mutate({
-                                       msg: text,
-                                       to: chat!.toUserId!,
-                                       senderId: user.userId!,
-                                   })}>
-                               <IconSend/>
-                           </Button>
-                       }/>
+                    size='lg' className='m-4 absolute bottom-4 w-8/12' rightSectionWidth={96 * 2 + 6}
+                    rightSection={
+                        <div className='flex flex-row space-x-2'>
+                            <Button variant='subtle' disabled={!text} loading={sendMutation.isLoading}
+                                onClick={() => sendMutation.mutate({
+                                    msg: text,
+                                    to: chat!.toUserId!,
+                                    senderId: user.userId!,
+                                })}>
+                                Start Bidding
+                            </Button>
+                            <Button variant='subtle' disabled={!text} loading={sendMutation.isLoading}
+                                onClick={() => sendMutation.mutate({
+                                    msg: text,
+                                    to: chat!.toUserId!,
+                                    senderId: user.userId!,
+                                })}>
+                                <IconSend />
+                            </Button>
+                        </div>
+                    } />
             </div>
         </div>
     );
