b55ee8d404df34d8e5a4d21d1fe08b17988d2911
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-06-25 09:46:37 +0545

    fix `too many renders` on chat

--- src/pages/chat/[assignmentId].tsx
+++ src/pages/chat/[assignmentId].tsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { api } from "~/utils/api";
-import { Button, Input, Loader } from '@mantine/core';
-import { IconSend, } from '@tabler/icons-react';
-import type { Message } from '@prisma/client'
-import { toast } from "react-hot-toast";
+import React, {useEffect, useRef, useState} from 'react';
+import {api} from "~/utils/api";
+import {Button, Input, Loader} from '@mantine/core';
+import {IconSend,} from '@tabler/icons-react';
+import type {Message} from '@prisma/client'
+import {toast} from "react-hot-toast";
 import pusher from "~/stores/pusher";
-import { Events } from "~/const/events";
-import { useAuth, useUser } from "@clerk/nextjs";
-import { useRouter } from "next/router";
-import { modals } from '@mantine/modals';
-import { QuantityInput } from '~/components/QuantityInput';
+import {Events} from "~/const/events";
+import {useAuth, useUser} from "@clerk/nextjs";
+import {useRouter} from "next/router";
+import {modals} from '@mantine/modals';
+import {QuantityInput} from '~/components/QuantityInput';
 
 function Index() {
     const router = useRouter()
     const assignmentId = parseInt(router.query.assignmentId?.toString() ?? '')
     const [msgs, setMsgs] = useState<Message[]>([])
-    const chatQuery = api.chat.chat.useQuery({ assignmentId }, {
+    const chatQuery = api.chat.chat.useQuery({assignmentId}, {
         onSuccess: data => setMsgs(data.messages)
     })
     const [text, setText] = useState('')
@@ -39,7 +39,7 @@ function Index() {
         pusher.unbind(idStr)
         pusher.subscribe(idStr).bind(Events.SEND_MESSAGE, (message: Message) => {
             setMsgs(msgs => [message, ...msgs])
-            messagesContainerRef.current?.scroll({ behavior: "smooth", top: 0 })
+            messagesContainerRef.current?.scroll({behavior: "smooth", top: 0})
         })
 
         return () => pusher.unsubscribe(chat.fromUserId!)
@@ -54,15 +54,18 @@ function Index() {
     })
 
     useEffect(() => {
-        messagesContainerRef.current?.scroll({ behavior: "smooth", top: 0 })
+        messagesContainerRef.current?.scroll({behavior: "smooth", top: 0})
     }, [msgs])
 
-    if (!chatQuery.isSuccess) return <center><Loader /></center>
+    const assignment = chat?.assignment
 
-    const assignment = chat!.assignment!
+    const max = Number(assignment?.budget.replace(/\D/g, ""))
 
-    const max = Number(assignment.budget.replace(/\D/g, ""))
-    setBiddingAmount(amount => amount || max)
+    useEffect(() => {
+        setBiddingAmount(amount => amount || max)
+    }, [max])
+
+    if (!chatQuery.isSuccess) return <center><Loader/></center>
 
     return (
         <div className='flex flex-row h-[80vh]'>
@@ -74,12 +77,12 @@ function Index() {
                         <div key={message.id} className='flex flex-col-reverse'>
                             {
                                 message.senderId != user.userId ? <div className='flex flex-row mb-1'>
-                                    <div
-                                        className='py-2 px-4 bg-blue-300 max-w-xl rounded-b-3xl rounded-tr-3xl'>{message.text}</div>
-                                    <div className='w-full' />
-                                </div> :
+                                        <div
+                                            className='py-2 px-4 bg-blue-300 max-w-xl rounded-b-3xl rounded-tr-3xl'>{message.text}</div>
+                                        <div className='w-full'/>
+                                    </div> :
                                     <div className='flex flex-row mb-1'>
-                                        <div className='w-full' />
+                                        <div className='w-full'/>
                                         <div
                                             className='py-2 px-4 bg-blue-300 max-w-xl rounded-t-3xl rounded-bl-3xl'>
                                             {message.text}
@@ -90,38 +93,38 @@ function Index() {
                     ))
                 }
                 <Input value={text} onChange={e => setText(e.target.value)} placeholder='Write something..'
-                    size='lg' className='m-4 absolute bottom-4 w-8/12' rightSectionWidth={96 * 2 + 6}
-                    rightSection={
-                        <div className='flex flex-row space-x-2'>
-                            <Button variant='subtle'
-                                onClick={() => modals.open({
-                                    title: 'Enter bidding amount',
-                                    children: <div className='flex flex-col space-y-6'>
-                                        <QuantityInput max={max} defaultValue={max} onChange={setBiddingAmount} />
-                                        <Button variant='subtle' loading={sendMutation.isLoading}
-                                            onClick={() => sendMutation.mutate({
-                                                msg: `New offer from ${auth.user?.firstName ?? ''}`,
-                                                to: chat!.toUserId!,
-                                                senderId: user.userId!,
-                                                isBid: true,
-                                                biddingPrice: biddingAmount,
-                                            })}>
-                                            Start Bidding
-                                        </Button>
-                                    </div>
-                                })}>
-                                Start Bidding
-                            </Button>
-                            <Button variant='subtle' disabled={!text} loading={sendMutation.isLoading}
-                                onClick={() => sendMutation.mutate({
-                                    msg: text,
-                                    to: chat!.toUserId!,
-                                    senderId: user.userId!,
-                                })}>
-                                <IconSend />
-                            </Button>
-                        </div>
-                    } />
+                       size='lg' className='m-4 absolute bottom-4 w-8/12' rightSectionWidth={96 * 2 + 6}
+                       rightSection={
+                           <div className='flex flex-row space-x-2'>
+                               <Button variant='subtle'
+                                       onClick={() => modals.open({
+                                           title: 'Enter bidding amount',
+                                           children: <div className='flex flex-col space-y-6'>
+                                               <QuantityInput max={max} defaultValue={max} onChange={setBiddingAmount}/>
+                                               <Button variant='subtle' loading={sendMutation.isLoading}
+                                                       onClick={() => sendMutation.mutate({
+                                                           msg: `New offer from ${auth.user?.firstName ?? ''}`,
+                                                           to: chat!.toUserId!,
+                                                           senderId: user.userId!,
+                                                           isBid: true,
+                                                           biddingPrice: biddingAmount,
+                                                       })}>
+                                                   Start Bidding
+                                               </Button>
+                                           </div>
+                                       })}>
+                                   Start Bidding
+                               </Button>
+                               <Button variant='subtle' disabled={!text} loading={sendMutation.isLoading}
+                                       onClick={() => sendMutation.mutate({
+                                           msg: text,
+                                           to: chat!.toUserId!,
+                                           senderId: user.userId!,
+                                       })}>
+                                   <IconSend/>
+                               </Button>
+                           </div>
+                       }/>
             </div>
         </div>
     );
