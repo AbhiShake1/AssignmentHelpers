48c516d70fccf45f5ee486bc53fcae8d4bb597e3
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-07-03 07:26:31 +0545

    add khalti key

--- src/utils/khalti.ts
+++ src/utils/khalti.ts
@@ -1,8 +1,7 @@
 import KhaltiCheckout from "khalti-checkout-web";
-import {toast} from "react-hot-toast";
 
 const config = {
-    "publicKey": "test_public_key_dc74e0fd57cb46cd93832aee0a507256",
+    "publicKey": "test_public_key_1afd1d2aa7cb409e9a89df9e83c10741",
     "productIdentity": "1234567890",
     "productName": "Drogon",
     "productUrl": "http://gameofthrones.com/buy/Dragons",
@@ -10,12 +9,10 @@ const config = {
         onSuccess() {
             console.log('success');
         },
-        // onError handler is optional
         onError: (err: any) => {
-            // handle errors
-            if (err) {
-                toast.error(err.toString())
-            }
+            // if (err) {
+            //     toast.error(err.toString())
+            // }
         }
     },
     // one can set the order of payment options and also the payment options based on the order and items in the array
