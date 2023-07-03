4d826959ca006f168b1ff3a53584810c9421012c
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-07-03 07:14:13 +0545

    design assignments grid when unlocked

--- src/pages/assignmentChats/[assignmentId].tsx
+++ src/pages/assignmentChats/[assignmentId].tsx
@@ -1,22 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { api } from "~/utils/api";
-import { ActionIcon, Button, Input, Loader, Navbar } from '@mantine/core';
+import React, {useEffect, useRef, useState} from 'react';
+import {api} from "~/utils/api";
+import {ActionIcon, Button, Input, Loader, Navbar} from '@mantine/core';
 import {IconArrowFork, IconAssembly, IconSend, IconUser,} from '@tabler/icons-react';
-import type { Message } from '@prisma/client'
-import { toast } from "react-hot-toast";
+import type {Message} from '@prisma/client'
+import {toast} from "react-hot-toast";
 import pusher from "~/stores/pusher";
-import { Events } from "~/const/events";
-import { useAuth } from "@clerk/nextjs";
-import { useRouter } from "next/router";
-import { useChatBarStyles } from "~/hooks/useChatBarStyles";
+import {Events} from "~/const/events";
+import {useAuth} from "@clerk/nextjs";
+import {useRouter} from "next/router";
+import {useChatBarStyles} from "~/hooks/useChatBarStyles";
 import {modals} from "@mantine/modals";
+import AssignmentPost from "~/components/AssignmentPost";
 
 function Index() {
     const router = useRouter()
     const assignmentId = parseInt(router.query.assignmentId?.toString() ?? '')
     const [msgs, setMsgs] = useState<Message[]>([])
     const chats = api.chat.assignmentChats.useQuery(assignmentId)
-    const { classes, cx } = useChatBarStyles();
+    const {classes, cx} = useChatBarStyles();
     const [active, setActive] = useState('')
     const [text, setText] = useState('')
     const messagesContainerRef = useRef<HTMLDivElement>(null);
@@ -55,26 +56,35 @@ function Index() {
     }, [user.userId, chat])
 
     useEffect(() => {
-        messagesContainerRef.current?.scroll({ behavior: "smooth", top: 0 })
+        messagesContainerRef.current?.scroll({behavior: "smooth", top: 0})
     }, [msgs])
 
-    if (!chats.isSuccess) return <center><Loader /></center>
+    if (!chats.isSuccess) return <center><Loader/></center>
 
     const links = chats.data.map((item) => (
         <a
-            className={cx(classes.link, { [classes.linkActive]: item.fromUserId === active })}
+            className={cx(classes.link, {[classes.linkActive]: item.fromUserId === active})}
             key={item.fromUserId}
             onClick={(event) => {
                 event.preventDefault();
                 setActive(item.fromUserId!);
             }}
         >
-            <IconUser className={classes.linkIcon} stroke={1.5} />
+            <IconUser className={classes.linkIcon} stroke={1.5}/>
             <span>{item.fromUser?.name || 'Anonymous'}</span>
-            {item?.assignmentUrls && <ActionIcon onClick={()=>{
+            {item?.assignmentUrls && <ActionIcon onClick={() => {
                 modals.open({
                     title: 'Assignment',
-                    children: item?.assignmentUnlocked ? <div>{item?.assignmentUrls}</div> : <div>pay to view</div>,
+                    children: item?.assignmentUnlocked ?
+                        <div className="m-8 p-8 grid w-full grid-rows-6 grid-flow-col gap-4 auto-cols-auto">
+                            {
+                                item?.assignmentUrls?.split(',').map((url, index) => (
+                                    <div key={index} className='rounded-md shadow-2xl w-36 h-36'>
+                                        <img src={url}/>
+                                    </div>
+                                ))
+                            }
+                        </div> : <div>pay to view</div>,
                 });
             }} className='scale-150 transition-transform duration-200'><IconAssembly className='ml-4'/></ActionIcon>}
         </a>
@@ -95,12 +105,12 @@ function Index() {
                             <div key={message.id} className='flex flex-col-reverse'>
                                 {
                                     message.senderId == '' ? <div className='flex flex-row mb-1'>
-                                        <div
-                                            className='py-2 px-4 bg-blue-300 max-w-xl rounded-b-3xl rounded-tr-3xl'>{message.text}</div>
-                                        <div className='w-full' />
-                                    </div> :
+                                            <div
+                                                className='py-2 px-4 bg-blue-300 max-w-xl rounded-b-3xl rounded-tr-3xl'>{message.text}</div>
+                                            <div className='w-full'/>
+                                        </div> :
                                         <div className='flex flex-row mb-1'>
-                                            <div className='w-full' />
+                                            <div className='w-full'/>
                                             <div
                                                 className='py-2 px-4 bg-blue-300 max-w-xl rounded-t-3xl rounded-bl-3xl'>
                                                 {message.text}
@@ -111,18 +121,18 @@ function Index() {
                         ))
                     }
                     <Input value={text} onChange={e => setText(e.target.value)} placeholder='Write something..'
-                        size='lg' className='m-4 absolute bottom-4 w-8/12'
-                        rightSection={
-                            <Button variant='subtle' disabled={!text} loading={sendMutation.isLoading}
-                                onClick={() => sendMutation.mutate({
-                                    msg: text,
-                                    to: chat.fromUserId || undefined,
-                                    senderId: '',
-                                    fromAdmin: true,
-                                })}>
-                                <IconSend />
-                            </Button>
-                        } />
+                           size='lg' className='m-4 absolute bottom-4 w-8/12'
+                           rightSection={
+                               <Button variant='subtle' disabled={!text} loading={sendMutation.isLoading}
+                                       onClick={() => sendMutation.mutate({
+                                           msg: text,
+                                           to: chat.fromUserId || undefined,
+                                           senderId: '',
+                                           fromAdmin: true,
+                                       })}>
+                                   <IconSend/>
+                               </Button>
+                           }/>
                 </div>
             }
         </div>
